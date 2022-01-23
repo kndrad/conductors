d9211f2b66f9802e7c34cd40b547780908ff4934
kndrad <kon.now@protonmail.com>
2022-01-23 20:48:25 +0100

    staticfiles in gitignore

--- .staticfiles/allocation_scroll.js
+++
@@ -1,9 +0,0 @@
-function scrollToDayNow() {
-    let date = getDateNow();
-    let element = `#rowDay${date.day}`;
-    scrollToElementUnderNavBar(element);
-}
-
-$(document).ready(function () {
-    scrollToDayNow()
-});
--- .staticfiles/scrolls.js
+++
@@ -1,7 +0,0 @@
-export function smoothScrollUnderHeader(element) {
-    window.scrollTo({
-        top: $(element).offset().top - $('#main-header').outerHeight(true),
-        behavior: 'smooth'
-    });
-}
-
