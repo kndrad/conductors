9e56e371047934cfd33e5963d26737af8213983e
kndrad <kon.now@protonmail.com>
2022-01-23 20:52:18 +0100

    changed storage

--- conductors/settings/config.py
+++ conductors/settings/config.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
     MEDIA_ROOT,
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
